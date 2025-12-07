065ddf9e59a1aca435fc10574a49a29146127062
Siddharth Devsani <devsanisiddharth27@gmail.com>
2025-12-07 14:02:55 +0530

    updated changes in backend dashboard2

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -60,20 +60,18 @@ router.post("/login",async(req,res)=>{
 
 //Protected Route(Dashboard)
 router.get("/dashboard",(req,res)=>{
-    try{
-        const authHeader = req.headers.authorization;
-        if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      return res.status(401).json({ message: "No token provided" });
-    }
+    try {
+    const token = req.headers.authorization?.split(" ")[1];
 
-        const token = authHeader.split(" ")[1];
+    if (!token) return res.status(400).json({ message: "No token provided" });
 
-          const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-          res.json({ message: "Welcome User", userId: decoded.id });
-
-
-    }
+    res.json({
+      message: "Welcome User",
+      userId: decoded.id,
+    });
+  }
     catch(err){
            console.log(err);
         res.status(401).json({message:"Invalid token"});
